bac36e175723b32a16c2d177d4ce9e0d2f54d845
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-12 18:38:49 +0600

    Load services data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hywmoi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,12 +27,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('services');
+
+    app.get("/services", async (req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection: {title: 1, price: 1, service_id: 1}
+      }
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
