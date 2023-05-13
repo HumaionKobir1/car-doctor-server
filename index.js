fb6c61d1d03154e5d868fdeb5d48837ef4f6d959
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-13 13:01:54 +0600

    Extract ooking service data

--- index.js
+++ index.js
@@ -29,22 +29,32 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
 
-    app.get("/services", async (req, res) => {
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    app.get('/services/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const options = {
-        projection: {title: 1, price: 1, service_id: 1}
-      }
-      const result = await serviceCollection.findOne(query, options);
-      res.send(result)
-    })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+
+        // bookings
+        app.post('/bookings', async(req, res) => {
+          const booking = req.body;
+        });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
