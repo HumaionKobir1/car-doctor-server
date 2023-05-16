7d00332c9406ce19786394ee416cc73518a73325
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-16 13:32:22 +0600

    verify and decode jwt token

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyJWT = (req, res, next) => {
+  console.log('hitting verify JWT');
+  console.log(req.headers.authorization)
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.send({error: true, message: 'unauthorize access'})
+      
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -68,7 +87,9 @@ async function run() {
 
 
         // bookings
-        app.get('/bookings', async(req, res) => {
+        app.get('/bookings', verifyJWT, async(req, res) => {
+          // console.log(req.headers.authorization);
+
           let query = {};
           if(req.query?.email){
             query = {email: req.query.email}
